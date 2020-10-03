9026b671a4adee4c76f86cf0bcc0b74c1b8c3425
Aoichaan0513 <aoichaan0513@gmail.com>
2020-10-03 23:45:17 +0900

    一歩進んだはず これで退化したらまじで笑う

--- src/main/kotlin/jp/aoichaan0513/A_TosoGame_Live/Listeners/Minecraft/onMove.kt
+++ src/main/kotlin/jp/aoichaan0513/A_TosoGame_Live/Listeners/Minecraft/onMove.kt
@@ -44,9 +44,15 @@ class onMove : Listener {
             if (p.isPlayerGroup) {
                 if (MissionManager.isMissions) {
                     if (MissionManager.isMission(MissionManager.MissionState.HUNTER_ZONE)) {
-                        Bukkit.broadcastMessage("[デバッグ] -> ${p.name} > from: ${isJoinedArea(from, hunterZoneBorder)}, to: ${isJoinedArea(to, hunterZoneBorder)}")
+                        Bukkit.broadcastMessage("[デバッグ] -> ${p.name} > エリア外から中へ from: ${isJoinedArea(from, hunterZoneBorder, true)}, to: ${isJoinedArea(to, hunterZoneBorder)}")
+                        Bukkit.broadcastMessage("[デバッグ] -> ${p.name} > エリア内から外へ from: ${isJoinedArea(from, hunterZoneBorder)}, to: ${isJoinedArea(to, hunterZoneBorder, true)}")
 
-                        if (!isJoinedArea(from, hunterZoneBorder) && isJoinedArea(to, hunterZoneBorder)) {
+                        if (isJoinedArea(from, hunterZoneBorder) && !isJoinedArea(to, hunterZoneBorder, true)) {
+                            // エリアから出たとき
+                            HunterZone.removeJoinedSet(p)
+
+                            Bukkit.broadcastMessage("[デバッグ] -> ${p.name} > 人数: ${HunterZone.joinedSetCount}, エリア退出")
+                        } else if (!isJoinedArea(from, hunterZoneBorder, true) && isJoinedArea(to, hunterZoneBorder)) {
                             // エリアに入ったとき
                             if (HunterZone.joinedSetCount < 3) {
                                 HunterZone.addJoinedSet(p)
@@ -59,15 +65,9 @@ class onMove : Listener {
                                 Bukkit.broadcastMessage("[デバッグ] -> ${p.name} > 人数: ${HunterZone.joinedSetCount}, エリア参加 -> ブロック")
                             }
 
-                        } else if (isJoinedArea(from, hunterZoneBorder)
-                                && (HunterZone.containsJoinedSet(p) && !isJoinedArea(to, hunterZoneBorder))) {
-                            // エリアから出たとき
-                            HunterZone.removeJoinedSet(p)
-
-                            Bukkit.broadcastMessage("[デバッグ] -> ${p.name} > 人数: ${HunterZone.joinedSetCount}, エリア退出")
                         }
                     } else {
-                        if (isJoinedArea(from, hunterZoneBorder) && !isJoinedArea(to, hunterZoneBorder)) {
+                        if (isJoinedArea(from, hunterZoneBorder) && !isJoinedArea(to, hunterZoneBorder, true)) {
                             HunterZone.removeJoinedSet(p)
 
                             Bukkit.broadcastMessage("[デバッグ] -> ${p.name} > 人数: ${HunterZone.joinedSetCount}, ボーダーアタック 2")
@@ -79,7 +79,7 @@ class onMove : Listener {
                         }
                     }
                 } else {
-                    if (isJoinedArea(from, hunterZoneBorder) && !isJoinedArea(to, hunterZoneBorder)) {
+                    if (isJoinedArea(from, hunterZoneBorder) && !isJoinedArea(to, hunterZoneBorder, true)) {
                         HunterZone.removeJoinedSet(p)
 
                         Bukkit.broadcastMessage("[デバッグ] -> ${p.name} > 人数: ${HunterZone.joinedSetCount}, ボーダーアタック 1")
@@ -150,16 +150,16 @@ class onMove : Listener {
                 || ((z1 == loc.blockZ || z2 == loc.blockZ) && isXAllowArea && isYAllowArea)
     }
 
-    fun isJoinedArea(loc: Location, borderConfig: IBorderConfig): Boolean {
+    fun isJoinedArea(loc: Location, borderConfig: IBorderConfig, isOutside: Boolean = false): Boolean {
         if (!borderConfig.isLocation(BorderType.POINT_1) || !borderConfig.isLocation(BorderType.POINT_2)) return false
 
         val loc1 = borderConfig.getLocation(BorderType.POINT_1)
         val loc2 = borderConfig.getLocation(BorderType.POINT_2)
 
-        return isJoinedArea(loc, loc1, loc2)
+        return isJoinedArea(loc, loc1, loc2, isOutside)
     }
 
-    fun isJoinedArea(loc: Location, loc1: Location, loc2: Location): Boolean {
+    fun isJoinedArea(loc: Location, loc1: Location, loc2: Location, isOutside: Boolean = false): Boolean {
         val x1 = loc1.blockX.coerceAtMost(loc2.blockX)
         val x2 = loc1.blockX.coerceAtLeast(loc2.blockX)
         val y1 = loc1.blockY.coerceAtMost(loc2.blockY) - 1
@@ -167,9 +167,11 @@ class onMove : Listener {
         val z1 = loc1.blockZ.coerceAtMost(loc2.blockZ)
         val z2 = loc1.blockZ.coerceAtLeast(loc2.blockZ)
 
-        val isXJoinedArea = loc.blockX in x1..x2
+        // val isXJoinedArea = loc.blockX in x1..x2
+        val isXJoinedArea = loc.blockX in if (isOutside) (x1 + 1) until x2 else x1..x2
         val isYJoinedArea = loc.blockY in y1..y2
-        val isZJoinedArea = loc.blockZ in z1..z2
+        // val isZJoinedArea = loc.blockZ in z1..z2
+        val isZJoinedArea = loc.blockZ in if (isOutside) (z1 + 1) until z2 else z1..z2
 
         return isXJoinedArea && isYJoinedArea && isZJoinedArea
     }
